4a4982449bb96ace1e6412140bd50f7961c90f6c
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-03-17 12:45:36 -0500

    images fixed again

--- js/app.js
+++ js/app.js
@@ -43,8 +43,8 @@ function app() {
 		render: function() {
 			return ( //<= must return only one parent element, though it can have many children
 				<div className="header-container">	
-	    			<img id="skyline" src="../dist/images/houston-skyline.jpg"/>
-	    			<img id="circle-logo"src="../dist/images/ironyardlogo.png"/>
+	    			<img id="skyline" src="./images/houston-skyline.jpg"/>
+	    			<img id="circle-logo"src="./images/ironyardlogo.png"/>
 	    		</div>		
 			)
 		}
@@ -70,7 +70,7 @@ function app() {
 				<div className="articleEl-container">
 					<p className="article-title">September 22 Starts a New Class of The Iron Yard Houston Students</p>
 					<p className="article-byline">By Brian Dorton, Campus Director at <a herf="https://www.theironyard.com">The Iron Yard</a> Houston</p>
-					<img src="../dist/images/classroom.jpg"/>
+					<img src="./images/classroom.jpg"/>
 					<p className="article-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit </p>
 				</div>
 			)
@@ -83,7 +83,7 @@ function app() {
 			return (
 				<div className="follow-container">
 					<p id="never-miss">Never miss a post!</p>
-					<img src="../dist/images/ironyardlogo.png"/>
+					<img src="./images/ironyardlogo.png"/>
 					<p id="tiyhouston">tiyhouston</p>
 					<p id="box-text">The Iron Yard | Houston</p>
 					<button>+ Follow</button>
